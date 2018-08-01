d0d3371f6123655ee3bfa50ce9db59ea7bc289b7
xdd <767403510@qq.com>
2018-08-01 16:55:37 +0800

    增加测试代码

---
+++ src/test/java/com/java/mobile/phone/login/service/impl/LoginServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.java.mobile.phone.login.service.impl;
+
+import org.junit.Test;
+
+public class LoginServiceImplTest {
+
+    @Test
+    public void test() {
+        System.err.println("----------------");
+    }
+
+}
