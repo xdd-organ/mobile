94d904e09be8c0bf90fa6f7d6efc679f95756ff1
xdd <767403510@qq.com>
2018-08-13 11:34:38 +0800

    logback日志配置，MDC注入参数配置，日志文件只输出info日志配置

--- src/main/java/com/java/mobile/phone/login/controller/LoginController.java
+++ src/main/java/com/java/mobile/phone/login/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.java.mobile.phone.login.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -12,8 +15,15 @@ import java.util.Map;
 @RestController
 public class LoginController {
 
-    @PostMapping("login")
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+//    @PostMapping("login")
+    @RequestMapping("login")
     public Map<String, Object> login() {
+        logger.debug("------ DEBUG -------------");
+        logger.info("------- INFO -------------");
+        logger.warn("-------- WARN -------------");
+        logger.error("------- ERROR -------------");
         return null;
     }
 }
