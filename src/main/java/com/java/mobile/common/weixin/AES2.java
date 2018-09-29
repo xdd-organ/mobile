4b6fa9ca257c77cb84968fbf48d10969cff6f5ef
tyetao <765068439@qq.com>
2018-09-29 21:08:02 +0800

    关锁

--- src/main/java/com/java/mobile/common/weixin/AES2.java
+++ src/main/java/com/java/mobile/common/weixin/AES2.java
@@ -169,10 +169,10 @@ public class AES2 {
 
     public static boolean verify(String plainText, String userKey, String signature) {
         try {
-            LOGGER.info("加密内容：{}", plainText);
+            LOGGER.info("加密内容：{}, userKey:{}, signature:{}", plainText, userKey, signature);
             String signedStr = signMd5(plainText + userKey);
             LOGGER.info("加密结果：{}", signedStr);
-            return signature.equals(signedStr);
+            return signature.equalsIgnoreCase(signedStr);
         } catch (Exception e) {
             LOGGER.error("验签异常", e);
             return false;
